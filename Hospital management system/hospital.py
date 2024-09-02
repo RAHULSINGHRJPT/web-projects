932b343c711cb7e25ff0c8efdb52ec0b8766d5d7
Rahul Singh Rajput <108928913+RAHULSINGHRJPT@users.noreply.github.com>
2024-09-03 02:39:27 +0530

    Add files via upload

---
+++ Hospital management system/hospital.py
@@ -0,0 +1,18 @@
+from tkinter import* 
+from tkinter import ttk
+import random
+import time
+import datetime
+from tkinter import messagebox
+import mysql.connector
+
+class Hospital:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Hospital Management System")
+        self.root.geometry("1540x800+0+0")
+
+
+root=Tk()
+ob=Hospital(root)
+root.mainloop()        
